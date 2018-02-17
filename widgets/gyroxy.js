b814c375573582cf08857e22857ad799c186b450
Armagan Amcalar <armagan@amcalar.com>
2018-02-17 12:26:14 +0100

    Use Gyro as the title for the gyro widget instead of accelerometer

--- widgets/gyroxy.js
+++ widgets/gyroxy.js
@@ -2,7 +2,7 @@ const contrib = require('blessed-contrib')
 const xymap = require('../lib/xymap')
 
 module.exports = function (grid) {
-    const gyroxx = grid.set(0, 11, 3, 1, xymap)
+    const gyroxx = grid.set(0, 11, 3, 1, xymap, { title: 'Gyro' })
 
     function update(val) {
         val = val.map(v => Math.min(Math.max(-7, v), 7) * 1.5);
